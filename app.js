e21ec6e92a80506b054928396e8443ab32d3f405
Fikri-luv <fatihalfikri5@gmail.com>
2020-04-20 20:04:48 +0700

    #24 express route params

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ var app = express();
 app.get('/', function(req,res){
 	res.send('this is the homepage')
 })
+
 app.get('/contact', function(req,res){
 	res.send('this is the contact page')
 })
 
-app.listen(3000);
+app.get('/profile/:id', function(req,res){
+	res.send(req.params)
+})
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  res.send(req.params)
+})
+
+app.listen(3000);    
