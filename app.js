73cb989f96ea3de3358bf915730fbf41a0b1f4c3
Fikri-luv <fatihalfikri5@gmail.com>
2020-04-21 19:36:27 +0700

    #29 query string

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ var app = express();
 
 app.set('view engine','ejs');
 
+// middleware usage
+	// express.static() is built in method on express module
+	app.use('/assets', express.static('assets'))
+
+	// app.use('/assets', function(req,res, next){
+	// 	console.log(req.url);
+	// 	next();
+	// })
 
 app.get('/', function(req,res){
+
 	// send Templating file
 	res.render('index')
 })
 
 app.get('/contact', function(req,res){
-	res.render('contact')
+	// console.log(req.query);
+	res.render('contact', {qs: req.query});
 })
  
-app.get('/profile /:name', function(req,res){
+app.get('/profile/:name', function(req,res){
 	var data = {age:29, jobs: 'ninja', hobbies:['sepak bola', 'voly', 'mancing','menjaring']}
 	res.render('template',{person:req.params.name, data: data})
 })
