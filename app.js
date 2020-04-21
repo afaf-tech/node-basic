f94b4412f22a3487c174531d87e1bc7c91d16510
Fikri-luv <fatihalfikri5@gmail.com>
2020-04-21 20:10:19 +0700

    #30 handling post request

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 
+
+var urlencodedParser = bodyParser.urlencoded({extended:false})
 app.set('view engine','ejs');
 
 // middleware usage
@@ -23,6 +25,13 @@ app.get('/contact', function(req,res){
 	// console.log(req.query);
 	res.render('contact', {qs: req.query});
 })
+
+// post method
+app.post('/contact',urlencodedParser, function(req,res){
+	console.log(req.body);
+	
+	res.render('contact-success', {data:req.body});
+});
  
 app.get('/profile/:name', function(req,res){
 	var data = {age:29, jobs: 'ninja', hobbies:['sepak bola', 'voly', 'mancing','menjaring']}
